a481f1d4033ae78096bdd53758fbedfee4b21d14
Xuân Kiên <kienlovend@gmail.com>
2021-05-19 18:30:17 +0700

    redirect login

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from "react";
+import { useRouter } from 'next/router';
 import Main from "../components";
 import { Typography } from "antd";
+import { getAccessToken } from './../utils/cookies';
 
 const { Title } = Typography;
 
 export default function Home() {
+  const accessToken = getAccessToken();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!accessToken) router.push('/login');
+  }, []);
+
+
   return (
     <div>
       <Main />
