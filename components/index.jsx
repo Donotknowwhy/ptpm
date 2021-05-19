4205c3a8bfd9d1743ca08d469e2d0da00a11d35b
Nam Phung <namphungptit204@gmail.com>
2021-05-20 01:13:46 +0700

    style nav

--- components/index.jsx
+++ components/index.jsx
@@ -10,7 +10,7 @@ function index(props) {
   return (
     <div>
       <Layout>
-        <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+        <Header style={{ position: "fixed", zIndex: 1, width: "100%" , height:"80px", backgroundColor: "#fff" , borderBottom: "1px solid #ccc"}}>
           <Navigation />
         </Header>
         <Layout>
