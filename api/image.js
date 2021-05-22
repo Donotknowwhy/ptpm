5e46a06f4a861c084ffef0d9484c0048b2c3fcc9
Xuân Kiên <kienlovend@gmail.com>
2021-05-23 00:44:24 +0700

    change url api

--- api/image.js
+++ api/image.js
@@ -3,5 +3,6 @@ import axios from "axios";
 export function getListImage(params) {
   // return axios.get(`https://picsum.photos/v2/list?page=${params}&limit=10`);
   // return axios.get(`http://209.126.13.177:13000/v1/flickr`) 
-  return axios.get(`http://localhost:3005/v1/images?page=${params}`) 
+  // return axios.get(`http://localhost:3005/v1/images?page=${params}`) 
+  return axios.get(`http://soc.laptrinhwebthatzui.me:13000/v1/images?page=${params}`) 
 }
