c8acadb339f5d46f103a94a87f5cc9b41eb4e475
Xuân Kiên <kienlovend@gmail.com>
2021-05-23 08:04:32 +0700

    style for content index

--- components/Content.jsx
+++ components/Content.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Card, Image, Row, Col, Avatar, Skeleton } from "antd";
+import { Card, Image, Row, Col, Avatar, Skeleton, Typography } from "antd";
 import { ExclamationCircleOutlined, UserOutlined } from '@ant-design/icons';
 import { getListImage } from "./../api/image";
 import Chatroom from "./Chatroom";
 import styles from "./Content.module.scss";
 const { Meta } = Card;
 
+
+const { Text, Link } = Typography;
 function Content() {
   const [listImg, setListImg] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
@@ -14,7 +16,6 @@ function Content() {
   const getData = (page) => {
     getListImage(page).then((res) => {
       console.log(res.data.data);
-      console.log(res.data.data.photos);
       setIsFetching(false);
       setListImg([...listImg, ...res.data.data.photos]);
     });
@@ -53,7 +54,6 @@ function Content() {
                 return (
                     <Row justify="end">
                     <Card
-                      title={"#"+items.id}
                       hoverable
                       style={{
                         width: 614,
@@ -64,10 +64,10 @@ function Content() {
                     >
                       <Meta
                         avatar={
-                          <Avatar icon={<UserOutlined />} />
+                          <Avatar size={40} src={items.avatar}  />
                         }
-                        title={items.photographer}
-                        description={items.photographer_url}
+                        title={<Link href={items.photographer_url} target="_blank">{items.photographer}</Link>}
+                        // description={<Text code>{'#'+items.id}</Text>}
                       />
                     </Card>
                     </Row>
